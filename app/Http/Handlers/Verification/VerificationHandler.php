81893269074390b384cbe5b31fdb50314c1f7c02
Obi Pascal <pascalobi83@gmail.com>
2024-12-07 15:58:15 +0100

    Correct carrier name casing for 9mobile in phone number verification

--- app/Http/Handlers/Verification/VerificationHandler.php
+++ app/Http/Handlers/Verification/VerificationHandler.php
@@ -34,7 +34,7 @@ class VerificationHandler
 				/* get the network provider or carrier */
 				$carrierMapper = PhoneNumberToCarrierMapper::getInstance();
 				$carrier = $carrierMapper->getNameForNumber($number, "en");
-                $carrier = $carrier === "9mobile" ? "etisalat" : $carrier;
+                $carrier = $carrier === "9mobile" ? "Etisalat" : $carrier;
 
 				/* Get country calling code */
 				$countryCallingCode = $NumberUtil->getCountryCodeForRegion($params["country_code"]);
